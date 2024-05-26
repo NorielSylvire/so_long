54f7d5e0c097737108da8ea0523818e1f1a030c5
NorielSylvire <fl4v1u5@yandex.com>
2024-05-26 11:53:01 +0200

    changed keycodes.h to use keysymdef.h

---
+++ src/include/so_long.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fhongu <fhongu@student.42madrid.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/26 00:10:54 by fhongu            #+#    #+#             */
+/*   Updated: 2024/05/26 11:49:45 by fhongu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <mlx.h>
+# include <ft_printf.h>
+
+# ifndef XK_MISCELLANY
+#  define XK_MISCELLANY
+# endif
+# ifndef XK_LATIN1
+#  define XK_LATIN1
+# endif
+# include <X11/keysymdef.h>
+
+#endif
